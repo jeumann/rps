182c7924a9b5db5e602a2fe84d9b317de86d1eed
jeumann <jeumann@ebay.com>
2013-04-17 17:16:35 +0200

    Added test combinations

--- src/test/java/com/ebay/rps/RockPaperScissorTest.java
+++ src/test/java/com/ebay/rps/RockPaperScissorTest.java
@@ -9,50 +9,6 @@ public class RockPaperScissorTest {
 
 	Game game = new Game(new RuleEvaluator());
 
-	@DataProvider(name = "Draw")
-	public Object[][] createDataForDraw() {
-		return new Object[][] { { Gesture.AIR }, { Gesture.FIRE },
-				{ Gesture.PAPER }, { Gesture.ROCK }, { Gesture.SCISSORS },
-				{ Gesture.SPONGE }, { Gesture.WATER } };
-	}
-	// TODO: Need to add the other combinations
-	@DataProvider(name = "Player1Wins")
-	public Object[][] createDataForPlayer1Wins() {
-		return new Object[][] { { Gesture.ROCK, Gesture.FIRE },
-				{ Gesture.ROCK, Gesture.SCISSORS },
-				{ Gesture.ROCK, Gesture.SPONGE },
-				{ Gesture.PAPER, Gesture.AIR },
-				{ Gesture.PAPER, Gesture.ROCK },
-				{ Gesture.PAPER, Gesture.WATER },
-				{ Gesture.FIRE, Gesture.SCISSORS },
-				{ Gesture.FIRE, Gesture.SPONGE },
-				{ Gesture.FIRE, Gesture.PAPER }
-		};
-	}
-	// TODO: Need to add the other combinations
-	@DataProvider(name = "Player2Wins")
-	public Object[][] createDataForPlayer2Wins() {
-		return new Object[][] {
-				{ Gesture.ROCK, Gesture.WATER },
-				{ Gesture.ROCK, Gesture.AIR },
-				{ Gesture.ROCK, Gesture.PAPER},
-				{ Gesture.PAPER, Gesture.SCISSORS},
-				{ Gesture.PAPER, Gesture.SPONGE},
-				{ Gesture.PAPER, Gesture.FIRE},
-				{ Gesture.WATER, Gesture.SPONGE},
-				{ Gesture.WATER, Gesture.PAPER},
-				{ Gesture.WATER, Gesture.AIR}
-		};
-	}
-
-	@DataProvider(name = "nullInputValues")
-	public Object[][] createDataForInputValues() {
-		return new Object[][] {
-				{ Gesture.ROCK, null },
-				{ null, Gesture.AIR }
-		};
-	}
-
 	@Test(dataProvider = "Draw")
 	public void checkDraws(Gesture gesture) {
 		GameResult gameResult = game.playRound(gesture, gesture);
@@ -74,5 +30,74 @@ public class RockPaperScissorTest {
 	@Test(dataProvider = "nullInputValues", expectedExceptions=RuntimeException.class)
 	public void checkForNullInputValues(Gesture gesturePlayer1, Gesture gesturePlayer2) {
 		game.playRound(gesturePlayer1, gesturePlayer2);
+	}
+
+	@DataProvider(name = "Draw")
+	public Object[][] createDataForDraw() {
+		return new Object[][] { { Gesture.AIR }, { Gesture.FIRE },
+				{ Gesture.PAPER }, { Gesture.ROCK }, { Gesture.SCISSORS },
+				{ Gesture.SPONGE }, { Gesture.WATER } };
+	}
+
+	@DataProvider(name = "Player1Wins")
+	public Object[][] createDataForPlayer1Wins() {
+		return new Object[][] {
+				{ Gesture.ROCK, Gesture.FIRE },
+				{ Gesture.ROCK, Gesture.SCISSORS },
+				{ Gesture.ROCK, Gesture.SPONGE },
+				{ Gesture.PAPER, Gesture.AIR },
+				{ Gesture.PAPER, Gesture.ROCK },
+				{ Gesture.PAPER, Gesture.WATER },
+				{ Gesture.FIRE, Gesture.SCISSORS },
+				{ Gesture.FIRE, Gesture.SPONGE },
+				{ Gesture.FIRE, Gesture.PAPER },
+				{ Gesture.SCISSORS, Gesture.SPONGE },
+				{ Gesture.SCISSORS, Gesture.PAPER },
+				{ Gesture.SCISSORS, Gesture.AIR },
+				{ Gesture.SPONGE, Gesture.WATER },
+				{ Gesture.SPONGE, Gesture.PAPER },
+				{ Gesture.SPONGE, Gesture.AIR },
+				{ Gesture.AIR, Gesture.WATER },
+				{ Gesture.AIR, Gesture.ROCK },
+				{ Gesture.AIR, Gesture.FIRE },
+				{ Gesture.WATER, Gesture.ROCK },
+				{ Gesture.WATER, Gesture.SCISSORS },
+				{ Gesture.WATER, Gesture.FIRE },
+		};
+	}
+
+	@DataProvider(name = "Player2Wins")
+	public Object[][] createDataForPlayer2Wins() {
+		return new Object[][] {
+				{ Gesture.ROCK, Gesture.WATER },
+				{ Gesture.ROCK, Gesture.AIR },
+				{ Gesture.ROCK, Gesture.PAPER},
+				{ Gesture.PAPER, Gesture.SCISSORS},
+				{ Gesture.PAPER, Gesture.SPONGE},
+				{ Gesture.PAPER, Gesture.FIRE},
+				{ Gesture.WATER, Gesture.SPONGE},
+				{ Gesture.WATER, Gesture.PAPER},
+				{ Gesture.WATER, Gesture.AIR},
+				{ Gesture.AIR, Gesture.PAPER},
+				{ Gesture.AIR, Gesture.SCISSORS},
+				{ Gesture.AIR, Gesture.SPONGE},
+				{ Gesture.SPONGE, Gesture.FIRE},
+				{ Gesture.SPONGE, Gesture.SCISSORS},
+				{ Gesture.SPONGE, Gesture.ROCK},
+				{ Gesture.SCISSORS, Gesture.FIRE},
+				{ Gesture.SCISSORS, Gesture.WATER},
+				{ Gesture.SCISSORS, Gesture.ROCK},
+				{ Gesture.FIRE, Gesture.AIR},
+				{ Gesture.FIRE, Gesture.WATER},
+				{ Gesture.FIRE, Gesture.ROCK},
+		};
+	}
+
+	@DataProvider(name = "nullInputValues")
+	public Object[][] createDataForInputValues() {
+		return new Object[][] {
+				{ Gesture.ROCK, null },
+				{ null, Gesture.AIR }
+		};
 	}
 }
